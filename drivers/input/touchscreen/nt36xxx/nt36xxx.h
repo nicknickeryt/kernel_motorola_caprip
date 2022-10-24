c0bd8bd5c16dc3f6dd23f244414037cacc31be73
NickNicker <nick4nicker@gmail.com>
2022-10-24 22:37:58 +0200

    fix touchscreen wakeup issuE

--- drivers/input/touchscreen/nt36xxx/nt36xxx.h
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.h
@@ -82,8 +82,13 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 #if WAKEUP_GESTURE
 extern const uint16_t gesture_key_array[];
 #endif
+<<<<<<< HEAD
 #define BOOT_UPDATE_FIRMWARE 1
 #define BOOT_UPDATE_FIRMWARE_NAME "novatek_ts_fw.bin"
+=======
+#define BOOT_UPDATE_FIRMWARE 0
+#define BOOT_UPDATE_FIRMWARE_NAME "nt36525c_novatek_ts_fw.bin"
+>>>>>>> 3b0dbb72fd7b (fix touchscreen wakeup issuE)
 
 //---ESD Protect.---
 #define NVT_TOUCH_ESD_PROTECT 0
